4c529bbb1b4b9be4cf24066bbc0de02ef6bf6657
Muhammad Awais <awaistopper20@gmail.com>
2022-10-28 20:53:39 +0500

    Index signatures

--- dist/index.js
+++ dist/index.js
@@ -1,20 +1,9 @@
 "use strict";
-class Account {
-    constructor(id, name, _balance) {
-        this.id = id;
-        this.name = name;
-        this._balance = _balance;
-    }
-    deposit(amount) {
-        if (amount <= 0)
-            throw new Error("Invalid Amount");
-        this._balance += amount;
-    }
-    get balance() {
-        return this._balance;
-    }
+class SeatAssignment {
 }
-let account = new Account(0, "Awais", 0);
-account.deposit(100);
-console.log(account.balance);
+let seats = new SeatAssignment();
+seats.A1 = 'Awais';
+seats.A2 = 'Ahmad';
+seats.A3 = 'Shahraiz';
+seats.A4 = 'Maaz';
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,9 @@
-class Account {
-    nickname?: string;
-
-    constructor(
-        public readonly id: number,
-        public name: string,
-        private _balance: number
-    ) {}
-
-    deposit(amount: number): void {
-        if (amount <= 0) throw new Error("Invalid Amount");
-        this._balance += amount;
-    }
-
-    get balance(): number {
-        return this._balance;
-    }
-
-    set balance(value: number) {
-        if (value <= 0) throw new Error("Invalid Amount");
-        this._balance = value;
-    }
+class SeatAssignment {
+    [seatNumber: string]: string;
 }
 
-let account = new Account(0, "Awais", 0);
-account.deposit(100);
-console.log(account.balance);
+let seats = new SeatAssignment();
+seats.A1 = 'Awais';
+seats.A2 = 'Ahmad';
+seats.A3 = 'Shahraiz';
+seats.A4 = 'Maaz';
