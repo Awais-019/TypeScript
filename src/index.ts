05ffb5bfed9de3b491793d25cf7a1831aaed6c22
Muhammad Awais <awaistopper20@gmail.com>
2022-10-29 19:46:46 +0500

    Extending generic classes

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,31 @@
-function echo<T>(value: T extends { name: string }): T {
-  return value;
+interface Product {
+    name: string;
+    price: number;
+}
+
+class Store<T>{
+    protected _objects: T[] = [];
+
+    add(obj: T): void {
+        this._objects.push(obj);
+    }
+}
+
+// Pass on the generic type parameter
+class CompressibleStore<T> extends Store<T>{
+    compress(){}
+}
+
+// Restrict the generic type parameter
+class SearchableStore<T extends { name: string }> extends Store<T>{
+    find(name: string): T | undefined {
+        return this._objects.find(obj => obj.name === name);
+    }
+}
+
+// Fix the generic type parameter
+class ProductStore extends Store<Product> {
+    filterByCategory(category: string): Product[] {
+        return [];
+    }
 }
